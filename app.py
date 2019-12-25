7325e60d54bf13f73508742b14703c53e6c72b5d
Abdi Nourkadi <abdi.nourkadi@gmail.com>
2019-12-25 22:44:38 +0400

    reworked css to grid layout

--- app.py
+++ app.py
@@ -13,10 +13,10 @@ import flask
 import dash
 import os
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 server = flask.Flask(__name__)
-app = dash.Dash(__name__, server=server, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, server=server)  # , external_stylesheets=external_stylesheets)
 
 # file_list = []
 # label_list = []
@@ -33,32 +33,35 @@ app = dash.Dash(__name__, server=server, external_stylesheets=external_styleshee
 # dict_img = {'image': file_list,
 #             'label': label_list}
 # df_img = pd.DataFrame(data=dict_img)
-#add index, tsne x & y
+# add index, tsne x & y
 # %%
 # csv is pickled and not human readable
 tsne = pd.read_pickle('data/tsne.csv')
 
 fig = px.scatter(tsne, x='x', y='y', color=tsne['label'],
-                 render_mode='webgl', height=500, width=1100, hover_data=['index']) \
+                 render_mode='webgl', height=900, width=900, hover_data=['index']) \
     .for_each_trace(lambda t: t.update(name=t.name.replace("label=", "")))
 
 fig.update_traces(marker_line=dict(width=1, color='DarkSlateGray'))
 fig.update_traces(marker=dict(size=8))
 
 # %%
-app.layout = html.Div(children=[
-    html.H1(children='T-SNE Viewer'),
+app.layout = html.Div(className="grid-container", children=[
 
-    html.Div(children='''
-        Img2Cluster: A web application for image clustering and labeling. Two
-    '''),
+    html.Div(className = 'title', children = [
+        html.H1(children='header'),
+        html.Div(children='div child'),
+    ]),
 
+    html.Div(className='sidebar', children='menu placeholder'),
     dcc.Graph(
+        className='tsnezone',
         id='2d-tsne',
         figure=fig
     ),
 
     dcc.Graph(
+        className='images',
         id='im-graph'
     )
 ])
@@ -67,20 +70,19 @@ app.layout = html.Div(children=[
 @app.callback(Output('im-graph', 'figure'),
               [Input('2d-tsne', 'clickData')])
 def display(click_data):
-    try:
+    if click_data:
         sample_idx = int(click_data['points'][0]['customdata'][0])
         sample_image = tsne['image'][sample_idx].reshape(28, 28)
         pic = px.imshow(sample_image, color_continuous_scale='gray')
-        pic.update_layout(coloraxis_showscale=False, height=400)
-        pic.update_layout(width=200, height=200, margin=dict(l=10, r=10, b=10, t=10))
+        pic.update_layout(coloraxis_showscale=False)
+        pic.update_layout(width=400, height=400)
         pic.update_xaxes(showticklabels=False).update_yaxes(showticklabels=False)
-
         return pic
-    except TypeError:
+    else:
         print("Caught TypeError")
         return {}
 
 
 if __name__ == '__main__':
-    #app.run_server(debug=True)#, host='0.0.0.0')
+    # app.run_server(debug=True)#, host='0.0.0.0')
     serve(server, host='0.0.0.0', port=8080, threads=10)
