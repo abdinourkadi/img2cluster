84cbeb4cc6de07a09bcd8d98415b5c9c79aa60a3
Abdi Nourkadi <abdi.nourkadi@gmail.com>
2019-12-28 23:41:05 +0400

    updated df to include full filepaths

--- app.py
+++ app.py
@@ -1,48 +1,63 @@
-# %%
 # -*- coding: utf-8 -*-
 from skimage.io import imread_collection
-import dash_html_components as html
-import dash_core_components as dcc
+from sklearn.manifold import TSNE
 import plotly.express as px
-from waitress import serve
-import json
+import pandas as pd
+import os
 
 from dash.dependencies import Input, Output
-import pandas as pd
+import dash_html_components as html
+import dash_core_components as dcc
+from waitress import serve
 import flask
 import dash
-import os
 
 server = flask.Flask(__name__)
 app = dash.Dash(__name__, server=server)
 
-# file_list = []
-# label_list = []
-#
-# for root, dirs, files in os.walk("data/images/", topdown=False):
-#     for folder in dirs:
-#         folder_path = os.path.join(root, folder, '*.jpg')
-#         print(folder_path)
-#         images = imread_collection(folder_path)
-#         for i in images:
-#             file_list.append(i.ravel())
-#             label_list.append(folder)
-#
-# dict_img = {'image': file_list,
-#             'label': label_list}
-# df_img = pd.DataFrame(data=dict_img)
-# add index, tsne x & y
-# %%
-# csv is pickled and not human readable
-tsne = pd.read_pickle('data/tsne.csv')
+path_list = []
+file_list = []
+label_list = []
 
+for root, dirs, files in os.walk("data\\images\\", topdown=False):
+    for folder in dirs:
+        folder_path = os.path.join(root, folder, '*.jpg')
+        print(folder_path)
+        images = imread_collection(folder_path)
+        path_list.extend(images.files)
+        for i in images[:10]:
+            file_list.append(i.ravel())
+            label_list.append(folder)
+
+# %%
+tsne_clf = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=300)
+tsne_results = tsne_clf.fit_transform(file_list)
+
+# %%
+index_list = range(len(file_list))
+x_values = tsne_results[:, 0]
+y_values = tsne_results[:, 1]
+
+# %%
+
+dict_img = {'index': index_list,
+            'label': label_list,
+            'x': x_values,
+            'y': y_values,
+            'paths': path_list,
+            'image': file_list}
+
+# %%
+df_img = pd.DataFrame(data=dict_img)
+df_img.to_pickle('data\\tsne_pickle.csv')
+tsne = pd.read_pickle('data\\tsne_pickle.csv')
+
+# %%
 fig = px.scatter(tsne, x='x', y='y', color=tsne['label'],
                  render_mode='webgl', height=700, width=600, hover_data=['index']) \
     .for_each_trace(lambda t: t.update(name=t.name.replace("label=", "")))
-fig.update_traces(marker_line=dict(width=1, color='DarkSlateGray'))
-fig.update_traces(marker=dict(size=8))
-fig.update_layout(paper_bgcolor='rgba(0,0,0,0)',
-                  plot_bgcolor='rgba(0,0,0,0)')
+fig.update_traces(marker_line=dict(width=1, color='DarkSlateGray'), marker=dict(size=8))
+fig.update_layout(paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)')
 
 # %%
 app.layout = html.Div(className="grid-container", children=[
@@ -80,7 +95,8 @@ def display(click_data):
             b=0,
             t=0,
             pad=0))
-        pic.update_xaxes(showticklabels=False).update_yaxes(showticklabels=False)
+        pic.update_xaxes(showticklabels=False)
+        pic.update_yaxes(showticklabels=False)
         return pic
     else:
         print("Caught TypeError")
